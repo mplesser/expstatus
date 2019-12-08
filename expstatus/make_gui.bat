2276c1a57d7484d6b9e7917a22c88d457fd5367e
Michael Lesser <mlesser@arizona.edu>
2019-12-07 19:02:37 -0700

    initial commit

---
+++ expstatus/make_gui.bat
@@ -0,0 +1,1 @@
+C:\Python36\Lib\site-packages\PyQt4\pyuic4 exposure_status.ui -o exposure_status_ui.py
---
+++ start_expstatus.bat
@@ -0,0 +1,1 @@
+PowerShell -WindowStyle Hidden ..\mont4k\venv\Scripts\python .\exposurestatus\exposure_status.pyw
