7bc1ce0d0c98e1ad8282a28976ceabce0e6edb3c
Michael Lesser <mlesser@arizona.edu>
2020-10-15 16:22:11 -0700

    cleanup

---
+++ README.md
@@ -0,0 +1,16 @@
+# azcam-expstatus
+
+*azcam-expstatus* is an *azcam* extension for displaying exposure status in a Qt (PySide2) window.
+
+## Documentation
+
+See https://mplesser.github.io/azcam-expstatus/
+
+
+## Installation
+
+Download from github: https://github.com/mplesser/azcam-expstatus.git.
+
+## Startup and configuration
+
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "azcam",
+]
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="azcam-expstatus",
+    version="20.1",
+    description="azcam extension for displaying exposure progress in Qt window",
+    long_description=long_description,
+    author="Michael Lesser",
+    author_email="mlesser@arizona.edu",
+    keywords="ccd imaging astronomy observation observatory",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=requirements,
+)
